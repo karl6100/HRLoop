31afd5783d8bdc30fd593205423c2c834f09cc52
karl6100 <karl6100@gmail.com>
2025-05-30 16:59:28 +0800

    add educational background

--- resources/views/employee/create.blade.php
+++ resources/views/employee/create.blade.php
@@ -166,6 +166,13 @@
                     </div>
                 </div>
             </div>
+            <div class="mb-6">
+            </div>
+            <hr class="mt-2 border-gray-300 dark:border-gray-600">
+            <div class="mb-6">
+                <h1 class="text-2xl font-bold text-gray-900 dark:text-white">Educational Background</h1>
+                <hr class="mt-2 border-gray-300 dark:border-gray-600">
+            </div>
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
 
