62e900d87fb498e251afa23b3a980b3eacf0e9da
karl6100 <karl6100@gmail.com>
2025-10-03 17:04:14 +0800

    update icons

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -16,13 +16,13 @@
                 <flux:navlist.group :heading="__('Platform')" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
                     @can('employees.list')
-                    <flux:navlist.item icon="users" :href="route('employee.index')" :current="request()->routeIs('employee.index')" wire:navigate>{{ __('Employee Directory') }}</flux:navlist.item>
+                    <flux:navlist.item icon="user-group" :href="route('employee.index')" :current="request()->routeIs('employee.index')" wire:navigate>{{ __('Employee Directory') }}</flux:navlist.item>
                     @endcan
                     <!-- <flux:navlist.item icon="briefcase" :href="route('employee.index')" :current="request()->routeIs('employee.index')" wire:navigate>{{ __('Company') }}</flux:navlist.item>
                     <flux:navlist.item icon="layout-grid" :href="route('employee.index')" :current="request()->routeIs('employee.index')" wire:navigate>{{ __('Departments') }}</flux:navlist.item> -->
                     @hasrole('admin')
-                    <flux:navlist.item icon="users" :href="route('users.index')" :current="request()->routeIs('users.index')" wire:navigate>{{ __('Manage Users') }}</flux:navlist.item>
-                    <flux:navlist.item icon="users" :href="route('roles.index')" :current="request()->routeIs('roles.index')" wire:navigate>{{ __('Roles and Permissions') }}</flux:navlist.item>
+                    <flux:navlist.item icon="user" :href="route('users.index')" :current="request()->routeIs('users.index')" wire:navigate>{{ __('Manage Users') }}</flux:navlist.item>
+                    <flux:navlist.item icon="key" :href="route('roles.index')" :current="request()->routeIs('roles.index')" wire:navigate>{{ __('Roles and Permissions') }}</flux:navlist.item>
                     @endhasrole
                 </flux:navlist.group>
             </flux:navlist>
@@ -30,13 +30,13 @@
             <flux:spacer />
 
             <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
+                <!-- <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
                 {{ __('Repository') }}
                 </flux:navlist.item>
 
                 <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
                 {{ __('Documentation') }}
-                </flux:navlist.item>
+                </flux:navlist.item> -->
             </flux:navlist>
 
             <!-- Desktop User Menu -->
